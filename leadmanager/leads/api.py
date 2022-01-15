d38e7e0bca0d2a3069b6f032ce80dbfdc800191e
Eric <erics1337@gmail.com>
2022-01-15 12:37:52 -0700

    Create Viewset

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,13 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
+# Viewsets allow you to create full CRUD api without having to specify explicit methods for the functionality ie generates urls for you
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Leads.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
